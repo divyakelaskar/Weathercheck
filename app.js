b59bdf03a3ae07210bd08a2998b4857329aa2800
Divya Kelaskar <divyakelaskar@gmail.com>
2022-01-02 19:00:12 +0530

    added constants and API key

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const form = document.querySelector(".top-banner form");
+
+const input = document.querySelector(".top-banner input");
+
+const msg = document.querySelector(".top-banner .msg");
+
+const list = document.querySelector(".ajax-section .cities");
+
+const apiKey = "4ca0daafb2e318165dde995d26fe64ea";
  
 form.addEventListener("submit", e => {
   e.preventDefault();
